9c962a0dcb4da8d03c1a2d7aac052ca3cb5bd651
Diyor Sattarov <sattarovdiyor@gmail.com>
2023-02-13 13:07:23 +0500

    feat: worked on OAuth

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys')
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hello: 'there' });
-});
-const PORT = process.env.PORT || 5000
+passport.use(
+  new GoogleStategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
+app.get(
+  '/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+)
+
+app.get('auth/google/callback', passport.authenticate('google'))
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT);
